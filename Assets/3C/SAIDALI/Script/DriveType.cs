b5aa9f5eb604018b2aeaff68358ad1c372e4ce54
afrae <adrien.fragai@e-artsup.net>
2021-03-02 14:53:30 +0100

    push

---
+++ Assets/3C/SAIDALI/Script/DriveType.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class DriveType : MonoBehaviour
+{
+[Serializable]
+public enum DriveTypes
+{
+	RearWheelDrive,
+	FrontWheelDrive,
+	AllWheelDrive
+}
+}
